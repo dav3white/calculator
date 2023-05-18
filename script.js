c835f73d5cc27fc4c73b76354e7684dc69968d1f
Dave White <dav3white@gmail.com>
2023-05-18 17:44:38 +0100

    Add JS display function

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let num1, num2, operator, display;
+let num1, num2, operator;
+const buttons = document.querySelectorAll('.button');
+let display = document.querySelector('#calculatorDisplay');
+display.innerHTML = 0;
+
+// Wait for the page to load
+window.addEventListener('load', function() {  
+    // Loop through each button and add a click event listener
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+        // Pass the text content of the clicked button to the updateDisplay() function
+        updateDisplay(button.textContent);
+        });
+    });
+  
+    // Function to update the display
+    function updateDisplay(btn) {
+        display.innerHTML = btn;
+    }
+});
+
+  
 
 function add(num1, num2) {
     return num1 + num2;
